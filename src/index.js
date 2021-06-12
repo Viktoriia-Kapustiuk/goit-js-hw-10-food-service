87ff6af3f08e204d6299bfb548796a76b421f5e0
Viktoriia-Kapustiuk <76262397+Viktoriia-Kapustiuk@users.noreply.github.com>
2021-06-12 19:56:34 +0300

    finished

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import './sass/main.scss';
+import menu from "./menu.json";
+import onGenerateMarkup from "./templates/meal-card.hbs";
+
+const listMenu = document.querySelector('.js-menu');
+listMenu.insertAdjacentHTML('beforeend', onGenerateMarkup(menu));
+
+const toggle = document.querySelector('#theme-switch-toggle');
+toggle.addEventListener('change', (e) => {
+    e.preventDefault();
+    if (localStorage.getItem('theme') === 'dark-theme') {
+        localStorage.setItem('theme', 'light-theme');
+    } else {
+        localStorage.setItem('theme', 'dark-theme');
+    }
+    createDarkMode();
+});
+
+function createDarkMode() {
+    if (localStorage.getItem('theme') === 'dark-theme') {
+        const body = document.querySelector('body');
+        body.classList.add('dark-theme');
+        toggle.setAttribute('checked', true);
+    } else {
+        document.querySelector('body').classList.remove('dark-theme');
+
+    }
+}
+createDarkMode();
